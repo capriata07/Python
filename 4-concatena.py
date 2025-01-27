00a002be91f7d1ce4e5268ae720aed8141c9f489
Rafael Capriata de Souza Azevedo <rafael.azevedo@economia.gov.br>
2025-01-27 16:55:34 -0400

    aprendendo concatenar

---
+++ 4-concatena.py
@@ -0,0 +1,18 @@
+nomeDoFilme = input("Nome do filme: ")
+anoFilme = int(input("informe o lançamento do filme: "))
+preco = float(input("valor do bilhete: "))
+
+
+#primeira alternativa
+print("Dados do filme.")
+print("================================")
+"""
+print("nome do filme: ", nomeDoFilme)
+print("No do filme: ", anoFilme)
+print("preço: ", preco)
+"""
+
+# segunda alternativa
+print(f"nome do filme:  {nomeDoFilme}")
+print(f"No do filme:  {anoFilme}")
+print(f"preço:  {preco}")
