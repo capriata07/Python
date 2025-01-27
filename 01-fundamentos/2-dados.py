1d433b10b972e42432048a32cc56b4c9b387d497
Rafael Capriata de Souza Azevedo <rafael.azevedo@economia.gov.br>
2025-01-27 16:12:16 -0400

    exemplo tipo de dados no python.

---
+++ 01-fundamentos/2-dados.py
@@ -0,0 +1,13 @@
+# em python a tipagem e dinamica
+# ele reconhece no automatico
+
+nome = "Rafael"
+idade = 40
+peso = 97.5
+admin = True
+
+
+print(type(nome))
+print(type(idade))
+print(type(peso))
+print(type(admin))
