9e9763d3c90b3a06d139ac1b0a60b74009aef71d
Rafael Capriata de Souza Azevedo <rafael.azevedo@economia.gov.br>
2025-01-27 16:21:10 -0400

    arquivo de capitura de dados..(input)

---
+++ 01-fundamentos/3-entrada.py
@@ -0,0 +1,15 @@
+nome = input("digite seu nome: ")
+idade = int(input("informe sua idade:"))
+peso = float(input("informe seu peso: "))
+
+
+print(f"""
+Dados Cadastrados :
+Nome: {nome}
+Idade: {idade}
+Peso: {peso}
+""")
+
+print(type(peso))
+print(type(idade))
+print(type(nome))
